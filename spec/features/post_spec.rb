960d26439420aae8ce6fc6a179ba947334a6519e
Michael Zhang <michaelzhang6284@gmail.com>
2018-07-19 00:14:49 +0200

    tests passed for new user creating post, associations created

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -8,11 +8,34 @@ describe 'navigate' do
       visit posts_path
       expect(page.status_code).to eq(200)
     end
-
     it 'it has a title of Posts' do
       visit posts_path
       expect(page).to have_content(/Posts/)
     end
-
   end
+
+  # mimic signing in a user, create a post, check to see if user is associated with that post
+  describe 'creation' do
+    before do
+      user = User.create(email:"test@test.com", password:"asdfasdf", first_name:"josh", last_name:"snow")
+      login_as(user, :scope => :user)
+      visit new_post_path
+    end
+    it 'has a new form that can be reached' do
+      expect(page.status_code).to eq(200)
+    end
+    it 'can be created from new form page' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Some rationale"
+      click_on "Save"
+      expect(page).to have_content("Some rationale")
+    end
+    it 'will have a user associated with it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User Association"
+      click_on "Save"
+      expect(User.last.posts.last.rationale).to eq("User Association")
+    end
+  end
+
 end
