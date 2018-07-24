d385715585c9a6831b216060a401e2d88f558b5f
Michael Zhang <michaelzhang6284@gmail.com>
2018-07-24 18:13:26 +0200

    seeds created for teams/users/posts/videos

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-50.times do |team|
-  Team.create(name: "#{team} team name")
+3.times do |team|
+  Team.create(name: "Team#{team+1}")
+  10.times do |user|
+    User.create(email:"asdf#{user+1}@asdf#{team+1}.com", password:"asdfasdf", first_name: "Joe#{user+1}", last_name: "Bloggs#{user+1}", team_id: Team.last.id)
+    3.times do |post|
+      Post.create(date: Date.today, rationale: "Lesson#{post+1}", user_id: User.last.id)
+      5.times do |video|
+        Video.create(heading: "Video#{video+1}", link: "https://www.youtube.com/watch?v=Xv1goNyR7PU", post_id: Post.last.id)
+      end
+    end
+  end
 end
 
 
-@user = User.create(email:"one@asdf.com", password:"asdfasdf", first_name: "joe", last_name: "bloggs", team_id:1)
 
 
-100.times do |post|
-  Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
-end
 
 
-puts "100 teams have been created"
-puts "one user has been created"
-puts "100 posts have been created"
+
+
+puts "3 teams have been created"
+puts "10 users per team has been created"
+puts "3 posts per user have been created"
+puts "5 videos per post have been created"
