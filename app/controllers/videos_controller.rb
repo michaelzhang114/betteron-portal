b3d38782f2c61da35bceab043712bcf9f56bac0d
Michael Zhang <michaelzhang6284@gmail.com>
2018-07-25 00:20:14 +0200

    tested videos index view

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,17 @@
 class VideosController < ApplicationController
   def index
-    @videos = Video.all
+    if params[:post_id]
+      @videos = Post.find(params[:post_id]).videos
+    else
+      @videos = Video.all
+    end
   end
+
+  private
+    def video_params
+      params.require(:video).permit(:heading, :link)
+    end
+    def set_video
+      @video = Video.find(params[:id])
+    end
 end
--- spec/features/video_spec.rb
+++ spec/features/video_spec.rb
@@ -20,15 +20,24 @@ describe 'navigate' do
     it 'has index page nested under post' do
       expect(page.status_code).to eq(200)
     end
-    # it 'has title of videos' do
-    #   expect(page).to have_content(/video/)
-    # end
-    # it 'has a list of videos' do
-    #   video1 = FactoryGirl.create(:video)
-    #   video2 = FactoryGirl.create(:second_video)
-    #   visit post_videos_path(post_id: @post)
-    #   expect(page).to have_content(/pJif4i9NRdI|piW99z4KsZk/)
-    # end
+    it 'has title of videos' do
+      expect(page).to have_content(/Videos/)
+    end
+    it 'does not show videos created by other users' do
+      video1 = FactoryGirl.create(:video)
+      video2 = FactoryGirl.create(:second_video)
+      visit post_videos_path(post_id: @post)
+      expect(page).to have_no_content(/pJif4i9NRdI|piW99z4KsZk/)
+    end
+    it 'shows videos created by current user' do
+      @index_user = User.create(email:"test@test.com", password: "asdfasdf", first_name: "joe", last_name: "bloggs", team_id: @team.id)
+      logout(:user)
+      login_as(@index_user, :scope => :user)
+      post = Post.create(date: Date.today, rationale: "xyz", user_id: @index_user.id)
+      video3 = Video.create(heading: "abcd", link: "https://www.youtube.com/watch?v=W1_D1Snja0E", post_id: post.id)
+      visit post_videos_path(post_id: post)
+      expect(page).to have_content(/abcd/)
+    end
   end
 
 
